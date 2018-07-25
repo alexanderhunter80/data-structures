09b3761ff13ffcd541f6a6f455164f03bd2d86ad
Alex Hunter <alexander.hunter.80@gmail.com>
2018-07-25 10:06:40 -0700

    minor commit

--- src/com/epoch/dataStructures/DataStructuresDriver.java
+++ src/com/epoch/dataStructures/DataStructuresDriver.java
@@ -8,31 +8,29 @@ public class DataStructuresDriver {
 		
 		System.out.println("Starting run in DataStructuresDriver");
 		
-		// so, uh, this doesn't work if the string length is less than 3...
-		// which OF COURSE is because there are only 36*36 = 1296 unique strings of length 2 :/ good job, brain!
 		DataStructuresTester tester = new DataStructuresTester(10000,4);
 		
 		
-////		// controls - Array and SLL
-////		
-//		tester.testControlArray();
-//		tester.testControlSinglyLinkedList();
-////		
-////		// lists
-////		
-//		tester.testDoublyLinkedList();
-////		
-////		// hashtables
-////
-//		tester.testLinearOpenAddressingHashtable();
-//		tester.testQuadraticOpenAddressingHashtable();
-//		tester.testPeriodicHashtable();
-//		
-//		// heaps
-//		
-//		tester.testMinHeap();
-//		
-//		// trees
+		// controls - Array and SLL
+		
+		tester.testControlArray();
+		tester.testControlSinglyLinkedList();
+		
+		// lists
+	
+		tester.testDoublyLinkedList();
+		
+		// hashtables
+
+		tester.testLinearOpenAddressingHashtable();
+		tester.testQuadraticOpenAddressingHashtable();
+		tester.testPeriodicHashtable();
+		
+		// heaps
+		
+		tester.testMinHeap();
+		
+		// trees
 		
 		tester.testBinarySearchTree();
 		
